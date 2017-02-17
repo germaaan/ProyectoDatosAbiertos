94684e3d29104e1b1a5476df4c154a1e5c2cb746
German Martinez <germaaan@gmail.com>
2017-02-17 11:41:16 +0100

    Playbook para instalar paquetes

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 ansible ckan --inventory-file=./ansible_hosts -u vagrant -m ping
+ansible ckan --inventory-file=./ansible_hosts -u vagrant -m shell -a 'uname -a'
+ansible-playbook --inventory-file=./ansible_hosts -become ckan.yml
