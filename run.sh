de47c6dc3583441d54e6f1b62ba67623ed22ede3
German Martinez <germaaan@gmail.com>
2017-03-17 11:46:02 +0100

    Añade comprobaciones en la instalación

--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+ssh-copy-id -i ~/.ssh/id_rsa.pub vagrant@192.168.33.10
 ansible ckan --inventory-file=./ansible_hosts -u vagrant -m ping
+ansible ckan --inventory-file=./ansible_hosts -u vagrant -m shell -a 'uname -a'
+ansible-playbook --inventory-file=./ansible_hosts -become ckan.yml
