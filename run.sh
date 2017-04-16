0e4dbca963142d9fa5f04db3866116a805ef4394
German Martinez <germaaan@gmail.com>
2017-04-16 23:20:41 +0200

    Arregla error en archivo de configuración

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
+ssh-keygen -f ~/.ssh/known_hosts -R 192.168.33.10
 ssh-copy-id -i ~/.ssh/id_rsa.pub vagrant@192.168.33.10
 ansible ckan --inventory-file=./hosts -u vagrant -m ping
 ansible ckan --inventory-file=./hosts -u vagrant -m shell -a 'uname -a'
-ansible-playbook --inventory-file=./hosts -become ckan.yml
+ansible-playbook --inventory-file=./hosts -become ckan2.yml
