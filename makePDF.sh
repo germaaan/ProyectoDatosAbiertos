ff14dbe3debdf33c26b7d510fca9a727b4387661
German Martinez <germaaan@gmail.com>
2017-07-05 14:51:29 +0200

    Corrige errata en script de generación del PDF

--- makePDF.sh
+++ makePDF.sh
@@ -2,7 +2,7 @@
 
 #Generación de PDF compilando el archivo LaTeX con pdflatex
 cd doc/text
-pdflatex -synctex=1 -interaction=nonstopmode -shell-escape -output-directory=../ project.tex
-pdflatex -synctex=1 -interaction=nonstopmode -shell-escape -output-directory=../ project.tex
-cd ..
+pdflatex -synctex=1 -interaction=nonstopmode -shell-escape -output-directory=../../ project.tex
+pdflatex -synctex=1 -interaction=nonstopmode -shell-escape -output-directory=../../ project.tex
+cd ../../
 rm {*.aux,*.lof,*.log,*.lol,*.lot,*.out,*.pyg,*.synctex.gz,*toc}
